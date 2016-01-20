2ed5ed41563d06778f8e1a19e4ade2ee3bf6924a
Christian Moritz <chrmoritz@users.noreply.github.com>
2016-01-20 19:48:36 +0100

    Initial commit

---
+++ jspm.config.js
@@ -0,0 +1,141 @@
+SystemJS.config({
+  transpiler: "plugin-typescript",
+  packages: {
+    "troxel": {
+      "format": "cjs",
+      "meta": {
+        "*.coffee": {
+          "loader": "coffee"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "github:*/*.json",
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {
+    "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
+    "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
+    "child_process": "github:jspm/nodelibs-child_process@0.2.0-alpha",
+    "coffee": "github:chrmoritz/system-coffee@0.2.0",
+    "events": "github:jspm/nodelibs-events@0.2.0-alpha",
+    "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
+    "http": "github:jspm/nodelibs-http@0.2.0-alpha",
+    "https": "github:jspm/nodelibs-https@0.2.0-alpha",
+    "jquery": "github:components/jquery@2.2.1",
+    "module": "github:jspm/nodelibs-module@0.2.0-alpha",
+    "os": "github:jspm/nodelibs-os@0.2.0-alpha",
+    "path": "github:jspm/nodelibs-path@0.2.0-alpha",
+    "plugin-typescript": "github:frankwallis/plugin-typescript@2.6.0",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "repl": "github:jspm/nodelibs-repl@0.2.0-alpha",
+    "stream": "github:jspm/nodelibs-stream@0.2.0-alpha",
+    "three": "github:mrdoob/three.js@r71",
+    "troxel": "github:chrmoritz/troxel@master",
+    "url": "github:jspm/nodelibs-url@0.2.0-alpha",
+    "util": "github:jspm/nodelibs-util@0.2.0-alpha",
+    "vm": "github:jspm/nodelibs-vm@0.2.0-alpha"
+  },
+  packages: {
+    "github:chrmoritz/system-coffee@0.2.0": {
+      "map": {
+        "coffee-script": "npm:coffee-script@1.10.0"
+      }
+    },
+    "github:chrmoritz/troxel@master": {
+      "map": {
+        "bluebird": "npm:bluebird@3.3.4",
+        "bootstrap": "github:twbs/bootstrap@3.3.6",
+        "coffee": "github:chrmoritz/system-coffee@0.2.0",
+        "css": "github:systemjs/plugin-css@0.1.20",
+        "jquery": "github:components/jquery@2.2.1",
+        "jszip": "github:Stuk/jszip@2.5.0",
+        "stats": "github:mrdoob/stats.js@r14",
+        "three": "github:mrdoob/three.js@r71",
+        "typeahead": "github:twitter/typeahead.js@0.11.1"
+      }
+    },
+    "github:frankwallis/plugin-typescript@2.6.0": {
+      "map": {
+        "typescript": "npm:typescript@1.7.5"
+      }
+    },
+    "github:jspm/nodelibs-buffer@0.2.0-alpha": {
+      "map": {
+        "buffer-browserify": "npm:buffer@4.5.0"
+      }
+    },
+    "github:jspm/nodelibs-http@0.2.0-alpha": {
+      "map": {
+        "http-browserify": "npm:stream-http@2.2.0"
+      }
+    },
+    "github:jspm/nodelibs-os@0.2.0-alpha": {
+      "map": {
+        "os-browserify": "npm:os-browserify@0.2.0"
+      }
+    },
+    "github:jspm/nodelibs-stream@0.2.0-alpha": {
+      "map": {
+        "stream-browserify": "npm:stream-browserify@2.0.1"
+      }
+    },
+    "github:jspm/nodelibs-url@0.2.0-alpha": {
+      "map": {
+        "url-browserify": "npm:url@0.11.0"
+      }
+    },
+    "github:twbs/bootstrap@3.3.6": {
+      "map": {
+        "jquery": "github:components/jquery@2.2.1"
+      }
+    },
+    "github:twitter/typeahead.js@0.11.1": {
+      "map": {
+        "jquery": "npm:jquery@2.2.2"
+      }
+    },
+    "npm:buffer@4.5.0": {
+      "map": {
+        "base64-js": "npm:base64-js@1.1.1",
+        "ieee754": "npm:ieee754@1.1.6",
+        "isarray": "npm:isarray@1.0.0"
+      }
+    },
+    "npm:readable-stream@2.0.6": {
+      "map": {
+        "core-util-is": "npm:core-util-is@1.0.2",
+        "inherits": "npm:inherits@2.0.1",
+        "isarray": "npm:isarray@1.0.0",
+        "process-nextick-args": "npm:process-nextick-args@1.0.6",
+        "string_decoder": "npm:string_decoder@0.10.31",
+        "util-deprecate": "npm:util-deprecate@1.0.2"
+      }
+    },
+    "npm:stream-browserify@2.0.1": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "readable-stream": "npm:readable-stream@2.0.6"
+      }
+    },
+    "npm:stream-http@2.2.0": {
+      "map": {
+        "builtin-status-codes": "npm:builtin-status-codes@2.0.0",
+        "inherits": "npm:inherits@2.0.1",
+        "to-arraybuffer": "npm:to-arraybuffer@1.0.1",
+        "xtend": "npm:xtend@4.0.1"
+      }
+    },
+    "npm:url@0.11.0": {
+      "map": {
+        "punycode": "npm:punycode@1.3.2",
+        "querystring": "npm:querystring@0.2.0"
+      }
+    }
+  }
+});
---
+++ libTroxel.js
@@ -0,0 +1,323 @@
+'use strict';
+var $ = require('jquery');
+module.exports = {
+  blueprints: null,
+  webgl: function() {
+    try {
+      var canvas = document.createElement('canvas');
+      return !!(window.WebGLRenderingContext && (canvas.getContext('webgl' || canvas.getContext('experimental-webgl'))));
+    } catch (e) {
+      return false;
+    }
+  },
+  renderBlueprint: function(blueprintId, domElement, options, cb) {
+    blueprintId = blueprintId.toLowerCase();
+    if (!Troxel.webgl()) {
+      console.warn("WebGL is not supported by your browser");
+      if (typeof cb === 'function') {
+        return cb(new Error("WebGl is not supported"));
+      }
+    }
+    $.getJSON('https://troxel.js.org/trove-blueprints/index.json', function(meta){
+      $.getJSON('https://troxel.js.org/trove-blueprints/' + meta.latest + '.json', function(data){
+        Troxel.blueprints = data;
+        var model = data[blueprintId];
+        var result = {
+          error: new Error("blueprintId " + blueprintId + " not found")
+        };
+        if (model != null) {
+          result = Troxel.renderBase64(model, domElement, options, blueprintId);
+        } else {
+          console.warn("blueprintId " + blueprintId + " not found");
+        }
+        if (typeof cb === 'function') {
+          cb(result.error, result.options);
+        }
+      });
+    });
+  },
+  renderBase64: function(base64, domElement, options, blueprintId) {
+    var Base64IO = require('troxel/Troxel.io.coffee!');
+    var Renderer = require('troxel/Renderer.coffee!');
+    if (options == null) {
+      options = {};
+    }
+    if (!Troxel.webgl()) {
+      console.warn("WebGL is not supported by your browser");
+      return {
+        error: new Error("WebGL is not supported by your browser")
+      };
+    }
+    domElement = $(domElement).empty().css('position', 'relative');
+    var io = null;
+    try {
+      io = new Base64IO(base64);
+    } catch (e) {
+      console.warn("passed String is not a valid base64 encoded voxel model");
+      return {
+        error: new Error("passed String is not a valid base64 encoded voxel model")
+      };
+    }
+    var renderer = new Renderer(io, true, domElement, options.renderMode || 0, options.renderWireframes || 0, options.rendererAntialias || true, options.rendererSSAO || false);
+    var THREE = require('three');
+    if (options.rendererClearColor != null) {
+      renderer.renderer.setClearColor(options.rendererClearColor);
+    }
+    if (options.ambientLightColor != null) {
+      renderer.ambientLight.color = new THREE.Color(options.ambientLightColor);
+    }
+    if (options.directionalLightColor != null) {
+      renderer.directionalLight.color = new THREE.Color(options.directionalLightColor);
+    }
+    if (options.directionalLightIntensity != null) {
+      renderer.directionalLight.intensity = options.directionalLightIntensity;
+    }
+    if ((options.directionalLightVector != null) && (options.directionalLightVector.x != null) && (options.directionalLightVector.y != null) && (options.directionalLightVector.z != null)) {
+      renderer.directionalLight.position.set(options.directionalLightVector.x, options.directionalLightVector.y, options.directionalLightVector.z).normalize();
+    }
+    if (options.pointLightColor != null) {
+      renderer.pointLight.color = new THREE.Color(options.pointLightColor);
+    }
+    if (options.pointLightIntensity != null) {
+      renderer.pointLight.intensity = options.pointLightIntensity;
+    }
+    if ((options.autoRotate == null) || options.autoRotate) {
+      renderer.controls.autoRotate = true;
+      renderer.controls.autoRotateSpeed = options.autoRotateSpeed || -4.0;
+    }
+    if ((options.controls != null) && !options.controls) {
+      renderer.controls.mode = false;
+    }
+    if ((options.noZoom != null) && options.noZoom) {
+      renderer.controls.noZoom = true;
+    }
+    if ((options.noPan != null) && options.noPan) {
+      renderer.controls.noPan = true;
+    }
+    if ((options.noRotate != null) && options.noRotate) {
+      renderer.controls.noRotate = true;
+    }
+    if ((options.showInfoLabel == null) || options.showInfoLabel) {
+      var link = blueprintId != null ? '#b=' + blueprintId : '#m=' + base64;
+      var info = $("<div><a href='http://chrmoritz.github.io/Troxel/" + link + "' target='_blank' class='troxelLink'>Open this model in Troxel</a></div>");
+      domElement.append(info.css({
+        position: 'absolute',
+        bottom: '0px',
+        width: '100%',
+        textAlign: 'center'
+      }));
+    }
+    var resultOptions = {};
+    var _resultOptions = {
+      rendererClearColor: 0x888888,
+      ambientLightColor: 0x606060,
+      directionalLightColor: 0xffffff,
+      directionalLightIntensity: 0.3,
+      directionalLightVector: {
+        x: -0.5,
+        y: -0.5,
+        z: 1
+      },
+      pointLightColor: 0xffffff,
+      pointLightIntensity: 0.7,
+      base64: base64,
+      controls: true,
+      autoRotate: true,
+      autoRotateSpeed: -4.0,
+      noZoom: false,
+      noPan: false,
+      noRotate: false,
+      renderMode: 0,
+      renderWireframes: 0
+    };
+    _resultOptions.blueprint = blueprintId || null;
+    Object.defineProperties(resultOptions, {
+      "base64": {
+        set: function(s) {
+          _resultOptions.base64 = s;
+          io = new Base64IO(s);
+          renderer.reload(io.voxels, io.x, io.y, io.z, true, false);
+        },
+        get: function() {
+          return _resultOptions.base64;
+        }
+      },
+      "blueprint": {
+        set: function(s) {
+          _resultOptions.blueprint = s.toLowerCase();
+          resultOptions.base64 = Troxel.blueprints[s.toLowerCase()];
+        },
+        get: function() {
+          return _resultOptions.blueprint;
+        }
+      },
+      "renderMode": {
+        set: function(s) {
+          _resultOptions.renderMode = s;
+          renderer.renderMode = s;
+          renderer.reload(io.voxels, io.x, io.y, io.z, false, false);
+        },
+        get: function() {
+          return _resultOptions.renderMode;
+        }
+      },
+      "renderWireframes": {
+        set: function(s) {
+          _resultOptions.renderWireframes = s;
+          renderer.renderWireframes = s;
+          renderer.reload(io.voxels, io.x, io.y, io.z, false, false);
+        },
+        get: function() {
+          return _resultOptions.renderWireframes;
+        }
+      },
+      "rendererClearColor": {
+        set: function(s) {
+          _resultOptions.rendererClearColor = s;
+          renderer.renderer.setClearColor(s);
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.rendererClearColor;
+        }
+      },
+      "ambientLightColor": {
+        set: function(s) {
+          _resultOptions.ambientLightColor = s;
+          renderer.ambientLight.color = new THREE.Color(s);
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.ambientLightColor;
+        }
+      },
+      "directionalLightColor": {
+        set: function(s) {
+          _resultOptions.directionalLightColor = s;
+          renderer.directionalLight.color = new THREE.Color(s);
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.directionalLightColor;
+        }
+      },
+      "directionalLightIntensity": {
+        set: function(s) {
+          _resultOptions.directionalLightIntensity = s;
+          renderer.directionalLight.intensity = s;
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.directionalLightIntensity;
+        }
+      },
+      "directionalLightVector": {
+        set: function(s) {
+          if ((s.x != null) && (s.y != null) && (s.z != null)) {
+            _resultOptions.directionalLightVector = s;
+            renderer.directionalLight.position.set(s.x, s.y, s.z).normalize();
+            renderer.render();
+          }
+        },
+        get: function() {
+          return _resultOptions.directionalLightVector;
+        }
+      },
+      "pointLightColor": {
+        set: function(s) {
+          _resultOptions.pointLightColor = s;
+          renderer.pointLight.color = new THREE.Color(s);
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.pointLightColor;
+        }
+      },
+      "pointLightIntensity": {
+        set: function(s) {
+          _resultOptions.pointLightIntensity = s;
+          renderer.pointLight.intensity = s;
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.pointLightIntensity;
+        }
+      },
+      "autoRotate": {
+        set: function(s) {
+          _resultOptions.autoRotate = s;
+          renderer.controls.autoRotate = s;
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.autoRotate;
+        }
+      },
+      "autoRotateSpeed": {
+        set: function(s) {
+          if (renderer.controls.autoRotate) {
+            _resultOptions.autoRotateSpeed = s;
+            renderer.controls.autoRotateSpeed = s;
+            renderer.render();
+          }
+        },
+        get: function() {
+          return _resultOptions.autoRotateSpeed;
+        }
+      },
+      "controls": {
+        set: function(s) {
+          _resultOptions.controls = s;
+          renderer.controls.mode = s;
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.controls;
+        }
+      },
+      "noZoom": {
+        set: function(s) {
+          _resultOptions.noZoom = s;
+          renderer.controls.noZoom = s;
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.noZoom;
+        }
+      },
+      "noPan": {
+        set: function(s) {
+          _resultOptions.noPan = s;
+          renderer.controls.noPan = s;
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.noPan;
+        }
+      },
+      "noRotate": {
+        set: function(s) {
+          _resultOptions.noRotate = s;
+          renderer.controls.noRotate = s;
+          renderer.render();
+        },
+        get: function() {
+          return _resultOptions.noRotate;
+        }
+      }
+    });
+    return {
+      error: null,
+      options: resultOptions
+    };
+  }
+};
+
+$(function() {
+  $('div[data-troxel-blueprint]').each(function() {
+    Troxel.renderBlueprint($(this).data('troxel-blueprint'), this, $(this).data('troxel-options'));
+  });
+  return $('div[data-troxel-base64]').each(function() {
+    Troxel.renderBase64($(this).data('troxel-base64'), this, $(this).data('troxel-options'));
+  });
+});
